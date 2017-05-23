4f6b82e67edd505971e04ccc53aa9b66c3e34cd9
vheimpel <vheimpel@gmail.com>
2017-05-23 00:05:18 +0000

    Fixes slide toggle focus bug

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,68 +1,11 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-// Fake data taken from tweets.json
-// var data = [
-//   {
-//     "user": {
-//             "name": "Newton",
-//             "avatars": {
-//                         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//                         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//                         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//                         },
-//             "handle": "@SirIsaac"
-//             },
-
-//     "content": {
-//               "text": "If I have seen further it is by standing on the shoulders of giants"
-//                 },
-//     "created_at": 1461116232227
-//                 },
-//               {
-
-
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense, donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//     },
-//     "created_at": 1461113796368
-//   }
-// ];
-
 function renderTweets(tweets) {
   var $container = $('#tweet-container');
-  var overallHtml = ""
+  var overallHtml = "";
   tweets.forEach((tweet) => {
     let html = createTweetElement(tweet);
     overallHtml += html;
   });
-$container.html(overallHtml)
+$container.html(overallHtml);
 }
 
 function createTweetElement(tweet) {
@@ -93,35 +36,38 @@ function loadTweets() {
       url: '/tweets',
       method: 'GET',
       success: function (tweetsJson) {
-        renderTweets(tweetsJson)
+        renderTweets(tweetsJson);
       }
-    })
+    });
 }
-loadTweets()
+loadTweets();
 
 
 $(document).ready(function() {
 
   $("#compose-button").click(function(){
-      $("#tweet-form").slideToggle(500);
-      $("#tweet-input").focus();
+      if (!$("#tweet-form").is(':visible')) {
+        $("#tweet-form").slideToggle(500);
+        $("#tweet-input").focus();
+      } else {
+        $("#tweet-form").slideToggle(500);
+      }
   });
 
   $("#tweet-form").on("submit", (event) => {
     event.preventDefault();
-    $("#tweet-error").text("")
+    $("#tweet-error").text("");
     const newTweet = $('#tweet-input').val();
-    console.log("We got to line 114")
 
     if (newTweet == "") {
-      $("#tweet-error").text("Please enter a tweet!")
+      $("#tweet-error").text("Please enter a tweet!");
       $(".counter").addClass("gone"); //Removes the counter
-      $("#tweet-error").removeClass("gone")
+      $("#tweet-error").removeClass("gone");
       return
       } else if (newTweet.length > 140) {
-        $("#tweet-error").text("Tweet too long!")
+        $("#tweet-error").text("Tweet too long!");
         $(".counter").addClass("gone"); //Removes the counter
-        $("#tweet-error").removeClass("gone")
+        $("#tweet-error").removeClass("gone");
         return
       }
 
@@ -138,12 +84,12 @@ $(document).ready(function() {
 
         const html = createTweetElement(tweet);
         $('#tweet-container').prepend(html);
-        $('#tweet-input').val('').focus(); //Clears the form after submittin
+        $('#tweet-input').val('').focus(); //Clears the form after submitting
         resetCounter();
         }).fail((error) => {
-          $("#tweet-error").text(error)
+          $("#tweet-error").text(error);
           console.error(error);
-        })
+        });
 
   });
 });
